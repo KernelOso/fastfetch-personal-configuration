8231f519aa5370ce55ab053affdddbaa13af26b0
Oscar D Prieto O <oscardpotsu@gmail.com>
2025-03-25 19:59:36 -0500

    Fix(fastfetch-random.sh): Fix .gitkeep detection
    Fix the detection error of the `.gitkeep` file. The script, when randomizing images, was incorrectly detecting the `.gitkeep` file, which occasionally caused the system's default logo to be displayed.

--- fastfetch-random.sh
+++ fastfetch-random.sh
@@ -2,6 +2,6 @@
 
 LOGO_DIR="$HOME/.config/fastfetch/logos"
 
-FASTFETCH_RANDOM_LOGO=$( find "$LOGO_DIR" -type f | shuf -n 1 )
+FASTFETCH_RANDOM_LOGO=$(find "$LOGO_DIR" -type f ! -name ".gitkeep" | shuf -n 1)
 
 fastfetch --logo "$FASTFETCH_RANDOM_LOGO" --logo-height 20
